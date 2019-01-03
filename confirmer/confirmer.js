2b98f24343810aa318bfab50937af984726ff64f
filmaj <maj.fil@gmail.com>
2019-01-03 14:45:55 -0600

    💼 license headers in place

--- confirmer/confirmer.js
+++ confirmer/confirmer.js
@@ -1,24 +1,36 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
 var request = require('request');
 var config = require('./config.json');
 var github_app = require('github-app');
 var openwhisk = require('openwhisk');
 
-function main(params) {
-	return new Promise((resolve, reject) => {
-		if(!params.agreement) {
-			reject(`NO AGREEMENT ID, SOMETHING IS WRONG!`);
-		}
+function main (params) {
+  return new Promise((resolve, reject) => {
+    if (!params.agreement) {
+      reject(`NO AGREEMENT ID, SOMETHING IS WRONG!`);
+    }
 
-		var agreementID = params.agreement.id;
+    var agreementID = params.agreement.id;
 
-		var response = {};
-			response = {
-				statusCode: 400,
-				//body: `something went horribly wrong ${clientID} ${config.signClientID}`
-				body: agreementID
-			};
-		resolve(response);
-	});
+    var response = {};
+    response = {
+      statusCode: 400,
+				// body: `something went horribly wrong ${clientID} ${config.signClientID}`
+      body: agreementID
+    };
+    resolve(response);
+  });
 }
 
 exports.main = main;
--- lookup/lookup.js
+++ lookup/lookup.js
@@ -1,13 +1,25 @@
-var request = require("request");
-var config = require("./config.json");
-var async = require("async");
-var parse = require("csv-parse");
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-function main(params) {
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+var request = require('request');
+var config = require('./config.json');
+var async = require('async');
+var parse = require('csv-parse');
+
+function main (params) {
   var agreements = [];
 
   return new Promise(function (resolve, reject) {
-    //Expects an array of agreement_ids for cla-bot/checker and a single agreement_id for cla-bot/confirmer
+    // Expects an array of agreement_ids for cla-bot/checker and a single agreement_id for cla-bot/confirmer
 
     if (params.agreements && params.agreements.constructor === Array) {
       agreements = params.agreements;
@@ -19,7 +31,7 @@ function main(params) {
       });
     }
 
-    //Get an access_token from a refresh_token for Adobe Sign APIs
+    // Get an access_token from a refresh_token for Adobe Sign APIs
     var options = {
       method: 'POST',
       url: 'https://api.na2.echosign.com/oauth/refresh',
@@ -54,11 +66,11 @@ function main(params) {
   });
 }
 
-function lookup(args, callback) {
+function lookup (args, callback) {
   var agreements = args.agreements;
   var usernames = [];
 
-  //Lookup github usernames for each agreement in the list and return the consolidated response
+  // Lookup github usernames for each agreement in the list and return the consolidated response
   async.each(agreements, function (agreement, callback) {
     lookupSingleAgreement(args, agreement, function (users) {
       usernames = Array.from(new Set(usernames.concat(users)));
@@ -66,16 +78,15 @@ function lookup(args, callback) {
     });
   }, function (err) {
     if (err) {
-      console.log("Error:" + err);
+      console.log('Error:' + err);
       callback(err);
     } else {
       callback(usernames);
     }
-
   });
 }
 
-function lookupSingleAgreement(args, agreement, callback) {
+function lookupSingleAgreement (args, agreement, callback) {
   var access_token = args.access_token;
 
   var options = {
@@ -89,28 +100,23 @@ function lookupSingleAgreement(args, agreement, callback) {
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
-    //Logic to parse CSV and get the value for Custom Field 8 or Custom Field 1 or githubUsername
-
-
+    // Logic to parse CSV and get the value for Custom Field 8 or Custom Field 1 or githubUsername
 
     parse(body.trim(), {
       columns: true
     }, function (err, records) {
       var usernames = [];
       var data = records[0];
-      if (data["Custom Field 8"] !== undefined) {
-        usernames = data["Custom Field 8"].split(/[\s,\n]+/);
-      } else if (data["Custom Field 1"] !== undefined) {
-        usernames.push(data["Custom Field 1"]);
+      if (data['Custom Field 8'] !== undefined) {
+        usernames = data['Custom Field 8'].split(/[\s,\n]+/);
+      } else if (data['Custom Field 1'] !== undefined) {
+        usernames.push(data['Custom Field 1']);
       } else if (data.githubUsername !== undefined) {
         usernames.push(data.githubUsername);
       }
       callback(usernames);
     });
-
-
   });
 }
-
 
 exports.main = main;
