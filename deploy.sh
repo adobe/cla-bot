af43606fb5e6a447eb00eb91b14bba25835772da
filmaj <maj.fil@gmail.com>
2019-08-14 11:52:40 -0500

    ♻️⚙️ added munging of utils.js based on environment deploys, also commenting / polishing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "deploy.sh pwd: $(pwd)"
+echo "pwd: $(pwd)"
 rm -rf dist
 mkdir dist
 ACTION="$1"
@@ -13,35 +13,15 @@ then
     echo "Action name must be one of 'checker', 'lookup' or 'setgithubcheck', exiting"
     exit 2
 fi
-ENV="$2"
-if [ -z "${ENV}" ]
-then
-    ENV="stage"
-fi
-ACTION_NAME="cla-${ACTION}"
-if [[ "$ENV" = "stage" ]]
-then
-    ACTION_NAME="cla-${ACTION}-stage"
-elif [[ "$ENV" = "production" ]]
-then
-    ACTION_NAME="cla-${ACTION}"
-fi
-WSK="wsk"
-if [ -e wsk ]
-then
-    WSK="./wsk"
-fi
-pushd $ACTION
-npm install
-popd
-cp -r "${ACTION}/${ACTION}.js" "${ACTION}/node_modules" "${ACTION}/package.json" dist/.
+
+# make sure we have all our credentials sorted
 if [ -e "${ACTION}/config.json" ]
 then
     echo "Using config.json from $ACTION/ dir"
     cp "${ACTION}/config.json" dist/config.json
 elif [ -e config.json ]
 then
-    echo "Using config.json from current dir"
+    echo "Using config.json from root dir"
     cp config.json dist/config.json
 elif [ ! -z "${GITHUB_APP_ID}" ] && [ ! -z "${GITHUB_KEY}" ] && [ ! -z "${SIGN_CLIENT_ID}" ] && [ ! -z "${SIGN_CLIENT_SECRET}" ] && [ ! -z "${SIGN_REFRESH_TOKEN}" ]
 then
@@ -59,9 +39,46 @@ else
     echo "No config.json nor appropriate environment variables found, bombing 💥"
     exit 1
 fi
-cp utils.js dist/.
+
+# set up action names based on environment
+ENV="$2"
+if [ -z "${ENV}" ]
+then
+    ENV="stage"
+fi
+ACTION_NAME="cla-${ACTION}"
+if [[ "$ENV" = "stage" ]]
+then
+    ACTION_NAME="cla-${ACTION}-stage"
+elif [[ "$ENV" = "production" ]]
+then
+    ACTION_NAME="cla-${ACTION}"
+fi
+echo "Environment assumed to be ${ENV} and deployed action name will be ${ACTION_NAME}"
+
+WSK="wsk"
+if [ -e wsk ]
+then
+    WSK="./wsk"
+fi
+echo "wsk binary used is ${WSK}"
+
+# get files for deployed action ready
+pushd $ACTION
+npm install
+popd
+cp -r "${ACTION}/${ACTION}.js" "${ACTION}/node_modules" "${ACTION}/package.json" dist/.
+# replace having the action load utils from the root of repo dir and instead use
+# flat directory structure of the deployed action
 sed -i.bak 's/\.\.\/utils\.js/\.\/utils\.js/' "dist/${ACTION}.js"
+cp utils.js dist/.
+# set the correct action name which may differ based on $ENV
+sed -i.bak "s/cla-$ACTION/$ACTION_NAME/" dist/utils.js
+
 pushd dist
+echo "`dist/` content listing:"
+ls -al
+echo "Zipping `dist`/..."
 zip -q -r "${ACTION}.zip" "${ACTION}.js" utils.js config.json package.json node_modules
 popd
 echo "Deploying ${ACTION_NAME}..."
@@ -73,3 +90,4 @@ else
     $WSK property set --apihost adobeioruntime.net --auth "${ADOBE_RUNTIME_AUTH}"
     $WSK action update "${ACTION_NAME}" --kind nodejs:10 "dist/${ACTION}.zip" --web true --apihost adobeioruntime.net --auth "${ADOBE_RUNTIME_AUTH}"
 fi
+echo "🌈✅"
