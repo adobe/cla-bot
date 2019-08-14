afd7e737a2359b5880f50021d99b1be31b545ac9
filmaj <maj.fil@gmail.com>
2019-08-14 18:17:51 -0500

    ✅♻️ refactor github integration tests a bit for reuse, add test for deny

--- test/integration/github.test.js
+++ test/integration/github.test.js
@@ -13,6 +13,47 @@ governing permissions and limitations under the License.
 const Octokit = require('@octokit/rest');
 
 if (!process.env.TEST_FOUR_PAC) throw new Error('environment variable `TEST_FOUR_PAC` not present, aborting. This is required for integration tests against GitHub.com.');
+if (!process.env.TEST_MAJ_PAC) throw new Error('environment variable `TEST_MAJ_PAC` not present, aborting. This is required for integration tests against GitHub.com.');
+
+function createBranch (github, user, repo, newBranch) {
+  return async function () {
+    console.log('\n[setup]');
+    console.log(`Getting ${user}/${repo} HEAD reference...`);
+    let headRef = await github.git.getRef({
+      owner: user,
+      repo,
+      ref: 'heads/master'
+    });
+    let headSha = headRef.data.object.sha;
+    console.log(`Creating branch ${newBranch} on ${user}/${repo}...`);
+    await github.git.createRef({
+      owner: user,
+      repo,
+      ref: `refs/heads/${newBranch}`,
+      sha: headSha
+    });
+    console.log(`Creating file on branch ${newBranch} on ${user}/${repo}...`);
+    await github.repos.createOrUpdateFile({
+      owner: user,
+      repo,
+      path: `${newBranch}.md`,
+      branch: newBranch,
+      message: 'adding new file',
+      content: Buffer.from('random contents').toString('base64')
+    });
+  };
+}
+function deleteBranch (github, user, repo, newBranch) {
+  return async function () {
+    console.log('\n[teardown]');
+    console.log(`Deleting branch ${newBranch} on ${user}/${repo}...`);
+    await github.git.deleteRef({
+      owner: user,
+      repo,
+      ref: `heads/${newBranch}`
+    });
+  };
+}
 
 function sleep (ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -33,6 +74,28 @@ async function waitForCheck (github, owner, repo, ref) {
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
 describe('github integration tests', () => {
+  describe('pull requests from user with no signed cla nor member of any org (account majtest)', () => {
+    const user = 'majtest';
+    const repo = 'cla-bot-playground';
+    const newBranch = '' + new Date().valueOf();
+    const github = new Octokit({
+      auth: process.env.TEST_MAJ_PAC
+    });
+    beforeAll(createBranch(github, user, repo, newBranch));
+    afterAll(deleteBranch(github, user, repo, newBranch));
+    it('should deny a pull request to an adobe repo from author with no signed cla nor org membership', async () => {
+      console.log('Creating pull request...');
+      let pr = await github.pulls.create({
+        owner: 'adobe',
+        repo,
+        title: `testing build ${newBranch}`,
+        head: `${user}:${newBranch}`,
+        base: 'master'
+      });
+      let suite = await waitForCheck(github, 'adobe', repo, pr.data.head.sha);
+      expect(suite.conclusion).not.toEqual('success');
+    });
+  });
   describe('pull requests from user in the adobe org (account adobeiotest4)', () => {
     const user = 'adobeiotest4';
     const repo = 'cla-bot-playground';
@@ -40,42 +103,9 @@ describe('github integration tests', () => {
     const github = new Octokit({
       auth: process.env.TEST_FOUR_PAC
     });
-    beforeAll(async () => {
-      console.log('\n[setup]');
-      console.log(`Getting ${user}/${repo} HEAD reference...`);
-      let headRef = await github.git.getRef({
-        owner: user,
-        repo,
-        ref: 'heads/master'
-      });
-      let headSha = headRef.data.object.sha;
-      console.log(`Creating branch ${newBranch} on ${user}/${repo}...`);
-      await github.git.createRef({
-        owner: user,
-        repo,
-        ref: `refs/heads/${newBranch}`,
-        sha: headSha
-      });
-      console.log(`Creating file on branch ${newBranch} on ${user}/${repo}...`);
-      await github.repos.createOrUpdateFile({
-        owner: user,
-        repo,
-        path: `${newBranch}.md`,
-        branch: newBranch,
-        message: 'adding new file',
-        content: Buffer.from('random contents').toString('base64')
-      });
-    });
-    afterAll(async () => {
-      console.log('\n[teardown]');
-      console.log(`Deleting branch ${newBranch} on ${user}/${repo}...`);
-      await github.git.deleteRef({
-        owner: user,
-        repo,
-        ref: `heads/${newBranch}`
-      });
-    });
-    it('a pull request to an adobe repo from an adobe org member should be approved', async () => {
+    beforeAll(createBranch(github, user, repo, newBranch));
+    afterAll(deleteBranch(github, user, repo, newBranch));
+    it('should approve a pull request to an adobe repo from an adobe org member', async () => {
       console.log('Creating pull request...');
       let pr = await github.pulls.create({
         owner: 'adobe',
