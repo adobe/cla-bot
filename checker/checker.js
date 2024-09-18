10592214ed41558a1023bc5a4b419721cc865379
Robert Snow <snowystinger@gmail.com>
2024-09-18 12:30:57 +1000

    Update checker.js

--- checker/checker.js
+++ checker/checker.js
@@ -25,8 +25,8 @@ gets fired from github pr creation/update webhook.
 const valid_pr_events = ['opened', 'reopened', 'synchronize'];
 
 async function main (params) {
-  let isMergeQueue = params.merge_group && params.action === 'checks_requested;
-  if (!params.pull_request || !valid_pr_events.includes(params.action) && !isMergeQueue) {
+  const isMergeQueue = params.merge_group && params.action === 'checks_requested;
+  if (!isMergeQueue && (!params.pull_request || !valid_pr_events.includes(params.action))) {
     return {
       statusCode: 202,
       body: 'Not a pull request being (re)opened or synchronized, ignoring'
